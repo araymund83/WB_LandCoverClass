45e3154eb15df69d484f1f92d4a4118c50499578
araymund83 <araymund83@gmail.com>
2021-01-04 17:36:23 +0000

    feat- adding wetlands and report

--- 00_init.R
+++ 00_init.R
@@ -5,7 +5,7 @@
 .starttime <- Sys.time()
 
 stopifnot(utils::packageVersion("googledrive") == "1.0.1")
-#googledrive::drive_auth(use_oob = TRUE)
+#googledrive::drive_auth(use_oob = TRUE)  ### use only for the first time 
 googledrive::drive_auth(email = "araymund83@gmail.com")
 
 #### for BorealCloud -------------------------------------------------------------
@@ -13,14 +13,14 @@ googledrive::drive_auth(email = "araymund83@gmail.com")
 workDirectory <- getwd()
 message("Your current temporary directory is ", tempdir())
 # Raster tmp
-scratchDirRas <- file.path("~/scratch/WB_LandCoverClass" )
+scratchDirRas <- file.path("/media/data/project/araymundo/scratch/WB_LandCoverClass")
 if(dir.create(scratchDirRas)) system(paste0("chmod -R 777 ", scratchDirRas), wait = TRUE) 
 raster::rasterOptions(default = TRUE)
 maxMemory <- 5e+12
 options(rasterMaxMemory = maxMemory, rasterTmpDir = scratchDirRas)
 
 activeDir <- if (dir.exists(scratchDirRas)) {
-  file.path("~/GITHUB/WB_LandCoverClass")
+  file.path(workDirectory)
 }
 
 #### Load libraries ------------------------------------------------------------
@@ -45,3 +45,24 @@ setPaths(cachePath = file.path(getwd(), "cache"),
          outputPath = checkPath(file.path(getwd(), "outputs"),
                                      create = TRUE))
 
+################################################################################
+##  setting options
+################################################################################
+options(
+  "spades.recoveryMode" = 2,
+  "spades.lowMemory" = TRUE,
+  "reproducible.inputPaths" = NULL,
+  "reproducible.cacheSaveFormat" = "qs",
+  "reproducible.qsPreset" = "fast",
+  "reproducible.useGDAL" = FALSE,
+  "reproducible.inputPaths" = NULL,
+  "reproducible.overwrite" = TRUE,
+  "reproducible.useMemoise" = TRUE, # Brings cached stuff to memory during the second run
+  "reproducible.useNewDigestAlgorithm" = TRUE,  # use the new less strict hashing algorithm
+  "reproducible.useCache" = TRUE,
+  "reproducible.cachePath" = file.path(scratchDirRas, "cache"),
+  "reproducible.showSimilar" = TRUE,
+  "spades.moduleCodeChecks" = FALSE, ## Turn off all module's code checking
+  "spades.useRequire" = TRUE, ##asuming all pkgs are installed correctly
+  "pemisc.useParallel" = TRUE
+)
--- 01_preamble.R
+++ 01_preamble.R
@@ -1,4 +1,3 @@
-
 library(magrittr)
 library(sf)
 library(sp)
@@ -22,7 +21,9 @@ bcrshp <- Cache(prepInputs,
                 targetCRS = targetCRS,
                 fun = "sf::st_read")
 
-
+#################################################################################
+## Canada Provinces 
+#################################################################################
 canProvs <- Cache(prepInputs,
                   "GADM",
                   fun = "base::readRDS",
@@ -34,43 +35,72 @@ canProvs <- Cache(prepInputs,
                   destinationPath = dPath) %>%
   st_as_sf(.) %>%
   st_transform(., targetCRS)
-
+#################################################################################
+## BCR for Western Boreal
+#################################################################################
 provs <- c("British Columbia", "Alberta", "Saskatchewan", "Manitoba")
+
+terrs <- c("Yukon", "Northwest Territories", "Nunavut")
+WB <- c(provs, terrs)
+
+bcrWB <- bcrshp[bcrshp$BCR %in% c(4, 6:8), ]
+provsWB <- canProvs[canProvs$NAME_1 %in% WB, ]
+
+studyArea <- reproducible::Cache(postProcess,
+                                 provsWB, studyArea= bcrWB, useSAcrs = TRUE,
+                                 cacheRepo = cPath, filename2 = NULL, overwrite = TRUE) 
+
+st_write(studyArea, "inputs/studyArea/BCR_WB.shp", driver = "ESRI Shapefile")
+
+#################################################################################
+## BCR6 subdivision
+#################################################################################
 provBCR6 <- c("British Columbia", "Alberta", "Saskatchewan", "Manitoba", 
               "Northwest Territories")
 provs2 <- c("British Columbia", "Alberta")
 provs3 <- c("Saskatchewan", "Manitoba")
 provs4 <- c("Northwest Territories")
-terrs <- c("Yukon", "Northwest Territories", "Nunavut")
-WB <- c(provs, terrs)
 
-bcrWB <- bcrshp[bcrshp$BCR %in% c(4, 6:8), ]
 bcr6 <- bcrshp[bcrshp$BCR %in% c(6), ]
-bcrWB2 <- postProcess(bcrWB, studyArea = studyArea, useSAcrs = TRUE)
-bcrWB2 <- as_Spatial(bcrWB2)
 
-st_write(bcrWB2, "inputs/studyArea/WB_BCR2.shp", driver = "ESRI Shapefile")
-provsWB <- canProvs[canProvs$NAME_1 %in% WB, ]
 provsBCR6 <- canProvs[canProvs$NAME_1 %in% provBCR6, ]
 NWT <- canProvs[canProvs$NAME_1 %in% provs4, ]
 ABBC <- canProvs[canProvs$NAME_1 %in% provs2, ]
 SKMB <- canProvs[canProvs$NAME_1 %in% provs3, ]
 
+bcr6SA <- reproducible::Cache(postProcess,
+                              provsBCR6,
+                              studyArea = bcr6, 
+                              useSAcrs = TRUE,
+                              cacheRepo = cPath, 
+                              filename2 = NULL, 
+                              overwrite = TRUE)
+## BCR6 Alberta - British Columbia
+bcr6ABBC <- reproducible::Cache(postProcess,
+                                bcr6SA, 
+                                studyArea = ABBC, 
+                                useSAcrs =  TRUE,
+                                cacheRepo = cPath,
+                                overwrite = TRUE)
 
-studyArea <- reproducible::Cache(postProcess,
-                   provsWB, studyArea= bcrWB, useSAcrs = TRUE,
-                   cacheRepo = cPath, filename2 = NULL, overwrite = TRUE) %>%
-# studyArea <- postProcess(provsWB, studyArea = bcrWB, useSAcrs = TRUE, cacheRepo = cPath,
-#                          filename2 = NULL, overwrite = TRUE) %>%
-  as_Spatial(.)
+## BCR6 North West Territories
+bcr6NWT <- reproducible::Cache(postProcess,
+                               NWT, 
+                               studyArea = bcr6SA, 
+                               useSAcrs = TRUE,
+                               cacheRepo = cPath, 
+                               filename2 = NULL,
+                               overwrite = TRUE)
+## BCR6 Saskatchewan - Manitoba
+bcr6SKMB <- postProcess(SKMB, studyArea = bcr6SA, useSAcrs = TRUE,
+                        cacheRepo = cPath, filename2 = NULL,
+                        overwrite = TRUE)
 
-
-bcr6SA <- postProcess(provsBCR6, studyArea = bcr6, useSAcrs = TRUE,
-                       cacheRepo = cPath, filename2 = NULL, overwrite = TRUE)
-bcr6ABBC <- postProcess(bcr6SA, studyArea = ABBC, useSAcrs =  TRUE)
-st_write(bcrWB2, "inputs/studyArea/BCR6_ABBC.shp", driver = "ESRI Shapefile")
-bcr6ABBC <- postProcess(bcr6SA, studyArea = ABBC, useSAcrs =  TRUE)
-st_write(bcr6ABBC, "inputs/studyArea/BCR6_ABBC.shp", driver = "ESRI Shapefile")
+## saving shapefiles (only do it once!)
+# st_write(bcr6SA, "inputs/studyArea/BCR6/BCR6.shp", driver = "ESRI Shapefile")
+# st_write(bcr6ABBC, "inputs/studyArea/BCR6/BCR6_ABBC.shp", driver = "ESRI Shapefile")
+# st_write(bcr6SKMB, "inputs/studyArea/BCR6/BCR6_SKMB.shp", driver = "ESRI Shapefile")
+# st_write(bcr6NWT, "inputs/studyArea/BCR6/BCR6_NWT.shp", driver = "ESRI Shapefile")
 
 
 bcr6SA$Province[bcr6SA$NAME_1 == "Alberta"] <- "AB"
@@ -79,35 +109,9 @@ bcr6SA$Province[bcr6SA$NAME_1 == "Manitoba"] <- "MB"
 bcr6SA$Province[bcr6SA$NAME_1 == "Saskatchewan"] <- "SK"
 bcr6SA$Province[bcr6SA$NAME_1 == "Northwest Territories"] <- "NWT"
 
-## this sf object has problems with rasterize, that is why geometry is being h
+## this sf object has problems when rasterize, that is why geometry is being h
 ## homogenize by using st_cast
 bcr6SA2 <- st_cast(bcr6SA,"MULTIPOLYGON")
-
-bcr6SA$Prov[bcr6SA$Province == "AB"] <- 1
-bcr6SA$Prov[bcr6SA$Province == "BC"] <- 2
-bcr6SA$Prov[bcr6SA$Province == "MB"] <- 3
-bcr6SA$Prov[bcr6SA$Province == "SK"] <- 4
-bcr6SA$Prov[bcr6SA$Province == "NWT"] <- 5
-
-as_Spatial(bcr6SA2)
-bcr6NWT <- postProcess(NWT, studyArea = bcr6SA, useSAcrs = TRUE,
-                              cacheRepo = cPath, filename2 = NULL,
-                               overwrite = TRUE)
-bcr6ABBC <- postProcess(ABBC, studyArea = bcr6SA, useSAcrs = TRUE,
-                              cacheRepo = cPath, filename2 = NULL,
-                               overwrite = TRUE)
-bcr6SKMB <- postProcess(SKMB, studyArea = bcr6SA, useSAcrs = TRUE,
-                              cacheRepo = cPath, filename2 = NULL,
-                               overwrite = TRUE)
-class(bcr6Split)
-
-plot(studyArea)
-
-
-st_write(studyArea, "inputs/studyArea/studyAreaWB.shp", driver = "ESRI Shapefile")
-st_write(bcr6ABBC, "inputs/studyArea/BCR6_ABBC.shp", driver = "ESRI Shapefile")
-st_write(bcr6SKMB, "inputs/studyArea/BCR6_SKMB.shp", driver = "ESRI Shapefile")
-st_write(bcr6NWT, "inputs/studyArea/BCR6_NWT.shp", driver = "ESRI Shapefile")
 
 
 #################################################################################
@@ -116,15 +120,24 @@ st_write(bcr6NWT, "inputs/studyArea/BCR6_NWT.shp", driver = "ESRI Shapefile")
 LCC05 <- reproducible::Cache(prepInputsLCC,year = 2005, 
                              studyArea = studyArea,
                              destinationPath = getPaths()$inputPath)
-## crop and mask with studyArea
-LCC05_6Ras <- postProcess(LCC05, studyArea = bcr6SA, useSAcrs = TRUE)
+## crop and mask with BCR6
+LCC05_6Ras <- reproducible::Cache(postProcess,
+                                  LCC05, 
+                                  studyArea = bcr6SA, 
+                                  useSAcrs = TRUE)
 
-  #LCC05Ras[LCC05Ras == 0] <- NA
+#################################################################################
+## Wetlands
+#################################################################################
+wetlandzip <- "https://drive.google.com/file/d/1R1AkkD06E-x36cCHWL4U5450mSDu_vD0/view?usp=sharing"
+wetlandWB <- Cache(prepInputs,
+                url = wetlandzip,
+                destinationPath = dPath,
+                targetFile = "CA_wetlands_post2000.tif",
+                userTags = c("wetlandWB")
+                )
+wetland6 <-  reproducible::Cache(postProcess,
+                                 wetlandWB, 
+                                 studyArea = bcr6SA, 
+                                 useSAcrs = TRUE)
 
-## convert to categorical values
-LCC05Ras <- as.factor(LCC05Ras)
-##add a look-up table for the vegetation classes
-fpath <- file.path(getwd(),"inputs", "LCC05ClookUp.csv" )
-levelRas<- read.csv(file = fpath, header = TRUE, sep = ",")
-
-levels(LCC05Ras) <- levelRas
