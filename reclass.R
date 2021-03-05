0e4c595d5203b17abe6732a7da1079dd28fe683c
Ana Raymundo <araymund83@gmail.com>
2021-03-05 20:51:34 +0000

    feat- file added

--- reclass.R
+++ reclass.R
@@ -8,8 +8,8 @@ vegTypeTable <- LandR::vegTypeGenerator(pixelCohortData, vegLeadingProportion = 
                                         mixedType = 2, sppEquiv = sppEquivalencies_CA,
                                         sppEquivCol = "WB", pixelGroupColName = "pixelGroup")
 vegTypeTable2 <- LandR::vegTypeGenerator(pixelCohortData, vegLeadingProportion = 0.8,
-                                        mixedType = 2, sppEquiv = sppEquivalencies_CA,
-                                        sppEquivCol = "WB", pixelGroupColName = "pixelGroup")
+                                         mixedType = 2, sppEquiv = sppEquivalencies_CA,
+                                         sppEquivCol = "WB", pixelGroupColName = "pixelGroup")
 
 
 vegTypeMap<- LandR::vegTypeMapGenerator(pixelCohortData, vegLeadingProportion = 0.8,
@@ -18,20 +18,20 @@ vegTypeMap<- LandR::vegTypeMapGenerator(pixelCohortData, vegLeadingProportion = 
                                         sppEquivCol = "WB", pixelGroupColName = "pixelGroup")
 
 ##subset the pixelCohortData and create a new column with the max age per pixelGroup
-newAgeCD <- vegTypeTable[,list(ageMax = max(age)), by = "pixelGroup"]
+newAgeCD <- vegTypeTable[,list(ageMax = max(age)), by = "pixelGroup"]  ## TODO: BIOMASS WEIGHTED MEAN ? 
 
 ##apply reclass function to the subset data.table
 newAgeCD <- ageReclass(newAgeCD)
 
 ## create a new  RasterLayer of Age reclassified
 ageGroupRas <- rasterizeReduced(reduced = newAgeCD, 
-                              fullRaster = outSimSppLayers[["pixelGroupMap"]],
-                              mapcode = "pixelGroup", newRasterCols =  "ageGroup")
+                                fullRaster = outSimSppLayers[["pixelGroupMap"]],
+                                mapcode = "pixelGroup", newRasterCols =  "ageGroup")
 
 ## subset the pixelCohortData and create a new column with the sum of Biomass(B) 
 ## per pixelGroup and RelB per pixelGroup & speciesCode
 sumBCD <- vegTypeTable[, list(sumB = sum(B)),
-                                  by =  "pixelGroup"]
+                       by =  "pixelGroup"]
 
 ## create the sumB RasterLayer
 sumBRas <- rasterizeReduced(reduced = sumBCD, 
@@ -44,21 +44,21 @@ vegTypeTable2[, relB := sum(B)/sumB, by =.(pixelGroup, speciesCode)]
 vegTypeTable2[is.na(relB) & sumB == 0, relB := 0]
 
 if(any(is.na(vegTypeTable2$relB)))
-   stop("Missing values in relative Biomass")
+  stop("Missing values in relative Biomass")
 
 ##subset to a smaller DT 
 vegTypes <- unique(vegTypeTable2[B > 0, .(pixelGroup,speciesCode, relB, sumB)])
 
 vegTypes$vegClass <- ifelse(vegTypes$speciesCode %in% c("Pinu_ban", "Pinu_con") & 
-  sum(vegTypes$relB >= pureCutoff)
-  & vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal","Betu_pap","Abie_las","Abie_bal")
-  & sum(vegTypes$relB >= .20), "pine",
-  ifelse(vegTypes$speciesCode %in% c("Pice_mar") &  sum(vegTypes$relB >= pureCutoff) &
-         vegTypes$speciesCode %in% deciSp & sum(vegTypes$relB >= 0.2), "BkSp",
-  ifelse(vegTypes$speciesCode %in% c("Pice_gla", "Abis_bal") & sum(vegTypes$relB >= pureCutoff), "WhSp",
-  ifelse(vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal", "Betu_pap") & sum(vegTypes$relB >= pureCutoff), "Deciduous",
-  ifelse(vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal", "Betu_pap") & sum(vegTypes$relB >= .20), "Mixedwood",
-  ifelse(vegTypes$speciesCode %in% c("Pice_mar") & sum(vegTypes$relB >= pureCutoff), "wetland", "NonVeg"))))))       
+                              sum(vegTypes$relB >= pureCutoff)
+                            & vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal","Betu_pap","Abie_las","Abie_bal")
+                            & sum(vegTypes$relB >= .20), "pine",
+                            ifelse(vegTypes$speciesCode %in% c("Pice_mar") &  sum(vegTypes$relB >= pureCutoff) &
+                                     vegTypes$speciesCode %in% deciSp & sum(vegTypes$relB >= 0.2), "BkSp",
+                                   ifelse(vegTypes$speciesCode %in% c("Pice_gla", "Abis_bal") & sum(vegTypes$relB >= pureCutoff), "WhSp",
+                                          ifelse(vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal", "Betu_pap") & sum(vegTypes$relB >= pureCutoff), "Deciduous",
+                                                 ifelse(vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal", "Betu_pap") & sum(vegTypes$relB >= .20), "Mixedwood",
+                                                        ifelse(vegTypes$speciesCode %in% c("Pice_mar") & sum(vegTypes$relB >= pureCutoff), "wetland", "NonVeg"))))))       
 
 
 a <- vegTypes[ , list(vegClass), by = "pixelGroup"]   
@@ -70,44 +70,42 @@ r <- SpaDES.tools::rasterizeReduced(reduced = a,
                                     mapcode = "pixelGroup", newRasterCols = "vegClass")
 
 if (studyArea == "ABBC"){
- 
- convertToVegType <- function(DT, groupingCol = "pixelGroup", pureCutoff = 0.8,
-                              deciSp = c("Popu_tre", "Popu_bal","Betu_pap","Abie_las",
-                                         "Abie_bal"),
-                              coniSp = c("Pinu_ban","Pinu_con", "Abie_bal")){
-   setkeyv(DT, cols = groupingCol)
-   cols <- c("speciesCode", "relB")
-  browser()
-##Pine dominant : dominated by Pinu_ban(jackpine) &/or Pinu_con (Lodgepole pine) > 80%
-## deciduous species less than 20% 
-  DT[, pine :=  all(.sumRelBs(c("Pinu_ban","Pinu_con"), .SD) >= pureCutoff,
-                    .sumRelBs(c('Popu_tre', 'Popu_bal','Betu_pap','Abie_las',
-                                  'Abie_bal'), .SD < 0.2)),
-                by = pixelGroup, .SDcols = cols]
-##Black Spruce (BkSp): dominated by Pice_mar (black spruce) > 80%
-  DT[, BkSp:= all(.sumRelBs("Pice_mar", .SD) >= pureCutoff,
+  
+  convertToVegType <- function(DT, groupingCol = "pixelGroup", pureCutoff = 0.8,
+                               deciSp = c("Popu_tre", "Popu_bal","Betu_pap")                                     
+                               coniSp = c("Pinu_ban","Pinu_con", "Abie_bal","Abie_las")){
+    setkeyv(DT, cols = groupingCol)
+    cols <- c("speciesCode", "relB")
+    browser()
+    ##Pine dominant : dominated by Pinu_ban(jackpine) &/or Pinu_con (Lodgepole pine) > 80%
+    ## deciduous species less than 20% 
+    DT[, pine :=  all(.sumRelBs(c("Pinu_ban","Pinu_con"), .SD) >= pureCutoff,
+                      .sumRelBs(c('Popu_tre', 'Popu_bal','Betu_pap'), .SD < 0.2)),
+       by = pixelGroup, .SDcols = cols]
+    ##Black Spruce (BkSp): dominated by Pice_mar (black spruce) > 80%
+    DT[, BkSp:= all(.sumRelBs("Pice_mar", .SD) >= pureCutoff,
                     .sumRelBs(deciSp), .SD > 0),
-                 by = pixelGroup, .SDcols = cols]
-   
-##White Spruce (WhSp): dominated by Pice_gla (white spruce) & Abis_bal (balsam fir) > 80%   
-  DT[, WhSp:= all(.sumRelBs(c("Pice_gla", "Abis_bal"), .SD) >= pureCutoff),
-                 by = pixelGroup, .SDcols = cols]
-##Deciduous : Popu_tre(trembling aspen) &/or Popu_bal (balsam poplar) &/or 
-##Betu_pap(white birch) > 80%   
-  DT[, deciduous:= all(.sumRelBs(c("Popu_tre","Popu_bal", "Betu_pap"),
-                                            .SD) >= pureCutoff,
-                       .sumRelBs(deciSp), .SD < 0.2),
-                 by = pixelGroup, .SDcols = cols]
-   
-##Mixedwood : Deciduous species more than 20%   
-  DT[, mixedwood:= all(.sumRelBs(c("Popu_tre","Popu_bal", "Betu_pap"),
-                                            .SD) >= 0.20,
-                       .sumRelBs(c(coniSp, "Pice_gla", "Pice_mar"), .SD < 0.2)),
-                 by = pixelGroup, .SDcols = cols]
-##Wetland :Black spruce dominated and  Deciduous species more than 20%    
-  DT[, wetland:= all(.sumRelBs("Pice_mar", .SD) >= pureCutoff),
-                 by = pixelGroup, .SDcols = cols]
- } 
+       by = pixelGroup, .SDcols = cols]
+    
+    ##White Spruce (WhSp): dominated by Pice_gla (white spruce) & Abis_bal (balsam fir) > 80%   
+    DT[, WhSp:= all(.sumRelBs(c("Pice_gla", "Abis_bal"), .SD) >= pureCutoff),
+       by = pixelGroup, .SDcols = cols]
+    ##Deciduous : Popu_tre(trembling aspen) &/or Popu_bal (balsam poplar) &/or 
+    ##Betu_pap(white birch) > 80%   
+    DT[, deciduous:= all(.sumRelBs(c("Popu_tre","Popu_bal", "Betu_pap"),
+                                   .SD) >= pureCutoff,
+                         .sumRelBs(deciSp), .SD < 0.2),
+       by = pixelGroup, .SDcols = cols]
+    
+    ##Mixedwood : Deciduous species more than 20%   
+    DT[, mixedwood:= all(.sumRelBs(c("Popu_tre","Popu_bal", "Betu_pap"),
+                                   .SD) >= 0.20,
+                         .sumRelBs(c(coniSp, "Pice_gla", "Pice_mar"), .SD < 0.2)),
+       by = pixelGroup, .SDcols = cols]
+    ##Wetland :Black spruce dominated and  Deciduous species more than 20%    
+    DT[, wetland:= all(.sumRelBs("Pice_mar", .SD) >= pureCutoff),
+       by = pixelGroup, .SDcols = cols]
+  } 
   
 }
 
@@ -117,8 +115,8 @@ if (studyArea == "ABBC"){
 #' @param DT data.table with columns 'speciesCode', 'relB'.
 .sumRelBs <- function(sppToMatch, DT) {
   browser()
-    DT[speciesCode %in% sppToMatch] %>%
-  
+  DT[speciesCode %in% sppToMatch] %>%
+    
     .[, .(speciesCode, relB)] %>%
     unique(.) %>%
     .$relB %>%
@@ -126,6 +124,6 @@ if (studyArea == "ABBC"){
 }
 
 out <- convertToVegType(DT = vegTypes,
-            groupingCol = "pixelGroup")
+                        groupingCol = "pixelGroup")
 
 
