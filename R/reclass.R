6e75d034da63fd9a726a8e79ab52d9cbe031713d
Alex Chubaty <achubaty@for-cast.ca>
2021-03-10 11:42:54 -0700

    cleaning up and testing reclass

--- R/reclass.R
+++ R/reclass.R
@@ -1,24 +1,28 @@
+library(SpaDES.core)
+library(SpaDES.tools)
+outSimSppLayers <- loadSimList("~/Downloads/simOutSpeciesLayers_ABBC.qs")
+
+# Ana's work below --------------------------------------------------------------------------------
+
 ## Add the pixel number to the cohortData
 pixelCohortData <- LandR::addNoPixel2CohortData(outSimSppLayers$cohortData, 
                                                 outSimSppLayers$pixelGroupMap,
                                                 doAssertion = getOption("LandR.assertions", TRUE))
 
-##Add vegetation type column to the cohortData table
+## Add vegetation type column to the cohortData table
 vegTypeTable <- LandR::vegTypeGenerator(pixelCohortData, vegLeadingProportion = 0.8,
-                                        mixedType = 2, sppEquiv = sppEquivalencies_CA,
+                                        mixedType = 2, sppEquiv = sppEquiv,
                                         sppEquivCol = "WB", pixelGroupColName = "pixelGroup")
-vegTypeTable2 <- LandR::vegTypeGenerator(pixelCohortData, vegLeadingProportion = 0.8,
-                                         mixedType = 2, sppEquiv = sppEquivalencies_CA,
+vegTypeTable2 <- copy(vegTypeTable)
+
+
+vegTypeMap <- LandR::vegTypeMapGenerator(pixelCohortData, vegLeadingProportion = 0.8,
+                                         mixedType = 2, sppEquiv = sppEquiv,
+                                         pixelGroupMap = outSimSppLayers$pixelGroupMap,
                                          sppEquivCol = "WB", pixelGroupColName = "pixelGroup")
 
-
-vegTypeMap<- LandR::vegTypeMapGenerator(pixelCohortData, vegLeadingProportion = 0.8,
-                                        mixedType = 2, sppEquiv = sppEquivalencies_CA,
-                                        pixelGroupMap = outSimSppLayers$pixelGroupMap,
-                                        sppEquivCol = "WB", pixelGroupColName = "pixelGroup")
-
 ##subset the pixelCohortData and create a new column with the max age per pixelGroup
-newAgeCD <- vegTypeTable[,list(ageMax = max(age)), by = "pixelGroup"]  ## TODO: BIOMASS WEIGHTED MEAN ? 
+newAgeCD <- vegTypeTable[, list(ageMax = max(age)), by = "pixelGroup"]  ## TODO: BIOMASS WEIGHTED MEAN ? 
 
 ##apply reclass function to the subset data.table
 newAgeCD <- ageReclass(newAgeCD)
@@ -30,8 +34,7 @@ ageGroupRas <- rasterizeReduced(reduced = newAgeCD,
 
 ## subset the pixelCohortData and create a new column with the sum of Biomass(B) 
 ## per pixelGroup and RelB per pixelGroup & speciesCode
-sumBCD <- vegTypeTable[, list(sumB = sum(B)),
-                       by =  "pixelGroup"]
+sumBCD <- vegTypeTable[, list(sumB = sum(B)), by =  "pixelGroup"]
 
 ## create the sumB RasterLayer
 sumBRas <- rasterizeReduced(reduced = sumBCD, 
@@ -39,26 +42,28 @@ sumBRas <- rasterizeReduced(reduced = sumBCD,
                             mapcode = "pixelGroup", newRasterCols =  "sumB")
 ## subset the pixelCohortData and create a new column with the sum of Biomass(B) & relB 
 ## per pixelGroup and RelB per pixelGroup & speciesCode
-vegTypeTable2[, sumB := sum(B), by =.(pixelGroup)]
-vegTypeTable2[, relB := sum(B)/sumB, by =.(pixelGroup, speciesCode)]
+vegTypeTable2[, sumB := sum(B), by = .(pixelGroup)]
+vegTypeTable2[, relB := sum(B)/sumB, by = .(pixelGroup, speciesCode)]
 vegTypeTable2[is.na(relB) & sumB == 0, relB := 0]
 
-if(any(is.na(vegTypeTable2$relB)))
+if (any(is.na(vegTypeTable2$relB))) {
   stop("Missing values in relative Biomass")
+}
 
-##subset to a smaller DT 
+## subset to a smaller DT 
 vegTypes <- unique(vegTypeTable2[B > 0, .(pixelGroup,speciesCode, relB, sumB)])
 
-vegTypes$vegClass <- ifelse(vegTypes$speciesCode %in% c("Pinu_ban", "Pinu_con") & 
-                              sum(vegTypes$relB >= pureCutoff)
-                            & vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal","Betu_pap","Abie_las","Abie_bal")
-                            & sum(vegTypes$relB >= .20), "pine",
-                            ifelse(vegTypes$speciesCode %in% c("Pice_mar") &  sum(vegTypes$relB >= pureCutoff) &
-                                     vegTypes$speciesCode %in% deciSp & sum(vegTypes$relB >= 0.2), "BkSp",
-                                   ifelse(vegTypes$speciesCode %in% c("Pice_gla", "Abis_bal") & sum(vegTypes$relB >= pureCutoff), "WhSp",
-                                          ifelse(vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal", "Betu_pap") & sum(vegTypes$relB >= pureCutoff), "Deciduous",
-                                                 ifelse(vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal", "Betu_pap") & sum(vegTypes$relB >= .20), "Mixedwood",
-                                                        ifelse(vegTypes$speciesCode %in% c("Pice_mar") & sum(vegTypes$relB >= pureCutoff), "wetland", "NonVeg"))))))       
+## TODO: romev this block -- it was Ana's 2nd attempt at reclassification
+# vegTypes$vegClass <- ifelse(vegTypes$speciesCode %in% c("Pinu_ban", "Pinu_con") & 
+#                               sum(vegTypes$relB >= pureCutoff)
+#                             & vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal","Betu_pap","Abie_las","Abie_bal")
+#                             & sum(vegTypes$relB >= .20), "pine",
+#                             ifelse(vegTypes$speciesCode %in% c("Pice_mar") &  sum(vegTypes$relB >= pureCutoff) &
+#                                      vegTypes$speciesCode %in% deciSp & sum(vegTypes$relB >= 0.2), "BkSp",
+#                                    ifelse(vegTypes$speciesCode %in% c("Pice_gla", "Abis_bal") & sum(vegTypes$relB >= pureCutoff), "WhSp",
+#                                           ifelse(vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal", "Betu_pap") & sum(vegTypes$relB >= pureCutoff), "Deciduous",
+#                                                  ifelse(vegTypes$speciesCode %in% c("Popu_tre", "Popu_bal", "Betu_pap") & sum(vegTypes$relB >= .20), "Mixedwood",
+#                                                         ifelse(vegTypes$speciesCode %in% c("Pice_mar") & sum(vegTypes$relB >= pureCutoff), "wetland", "NonVeg"))))))       
 
 
 a <- vegTypes[ , list(vegClass), by = "pixelGroup"]   
