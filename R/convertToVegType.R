c81563e8a7260dddcb7243c8f2ede3d8b9058b8e
Ana Raymundo <araymund83@gmail.com>
2021-03-23 01:38:55 +0000

    feat- convertToVegType function working for ABBC

--- R/convertToVegType.R
+++ R/convertToVegType.R
@@ -1,52 +1,53 @@
-convertToVegType <- function(DT, groupingCol = "pixelGroup", pureCutoff = 0.8,
-                             deciSp = c("Popu_tre", "Popu_bal","Betu_pap","Abie_las",
-                                        "Abie_bal"),
-                             coniSp = c("Pinu_ban","Pinu_con", "Abie_bal")){
-  setkeyv(DT, cols = groupingCol)
-  cols <- c("speciesCode", "relB")
-  browser()
-  ##Pine dominant : dominated by Pinu_ban(jackpine) &/or Pinu_con (Lodgepole pine) > 80%
-  ## deciduous species less than 20% 
-  DT[, pine :=  all(.sumRelBs(c("Pinu_ban","Pinu_con"), .SD) >= pureCutoff,
-                    .sumRelBs(c('Popu_tre', 'Popu_bal','Betu_pap','Abie_las',
-                                'Abie_bal'), .SD < 0.2)),
-     by = pixelGroup, .SDcols = cols]
-  ##Black Spruce (BkSp): dominated by Pice_mar (black spruce) > 80%
-  DT[, BkSp:= all(.sumRelBs("Pice_mar", .SD) >= pureCutoff,
-                  .sumRelBs(deciSp), .SD > 0),
-     by = pixelGroup, .SDcols = cols]
-  
-  ##White Spruce (WhSp): dominated by Pice_gla (white spruce) & Abis_bal (balsam fir) > 80%   
-  DT[, WhSp:= all(.sumRelBs(c("Pice_gla", "Abis_bal"), .SD) >= pureCutoff),
-     by = pixelGroup, .SDcols = cols]
-  ##Deciduous : Popu_tre(trembling aspen) &/or Popu_bal (balsam poplar) &/or 
-  ##Betu_pap(white birch) > 80%   
-  DT[, deciduous:= all(.sumRelBs(c("Popu_tre","Popu_bal", "Betu_pap"),
-                                 .SD) >= pureCutoff,
-                       .sumRelBs(deciSp), .SD < 0.2),
-     by = pixelGroup, .SDcols = cols]
-  
-  ##Mixedwood : Deciduous species more than 20%   
-  DT[, mixedwood:= all(.sumRelBs(c("Popu_tre","Popu_bal", "Betu_pap"),
-                                 .SD) >= 0.20,
-                       .sumRelBs(c(coniSp, "Pice_gla", "Pice_mar"), .SD < 0.2)),
-     by = pixelGroup, .SDcols = cols]
-  ##Wetland :Black spruce dominated and  Deciduous species more than 20%    
-  DT[, wetland:= all(.sumRelBs("Pice_mar", .SD) >= pureCutoff),
-     by = pixelGroup, .SDcols = cols]
+convertToVegType <- function(DT, pureCutoff = 0.8,
+                             pine  = c("Pinu_ban", "Pinu_con"),
+                             deciSp = c("Popu_tre", "Popu_bal","Betu_pap"),
+                             coniSp = c("Pinu_ban", "Pinu_con", "Abie_bal", "Abie_las")) {
+  ## TODO: use factors or integers for vegClass ???
+  zeroCutoff = 0.05
+  if (DT$leading2 %in% pine &&
+      .sumRelBs(deciSp, DT) < 0.20) {
+    ## Pine dominant: leading species are Pinu_ban(jackpine) &/or Pinu_con (Lodgepole pine)
+    ## deciduous species less than 20%
+    "Pine"
+  } else if (DT$leading2 %in% c("Pice_mar") &&
+             (.sumRelBs("Lari_lar", DT) < zeroCutoff ||
+              .sumRelBs(c("Popu_tre", "Popu_bal", "Abie_bal", "Pinu_ban", "Pinu_con"), DT) >= zeroCutoff)) {
+    #   ## Black Spruce (BkSp): leading species is Pice_mar (black spruce)
+    #   ### deciduous species is more than 0
+    "BkSp"
+  } else if (.sumRelBs(c("Pice_gla", "Abie_bal"), DT) >= 0.8) {
+    ## White Spruce (WhSp): dominated by Pice_gla (white spruce) & Abie_bal (balsam fir) > 80%
+    "WhSp"
+  } else if (.sumRelBs(deciSp, DT) >= 0.8) {
+    ## Deciduous: Popu_tre(trembling aspen) &/or Popu_bal (balsam poplar) &/or
+    ## Betu_pap(white birch) > 80%
+    "Decid"
+  } else if (.sumRelBs(deciSp, DT) >= 0.2 &&
+             .sumRelBs(c(coniSp, "Pice_gla", "Pice_mar"), DT) >= 0.2) {
+    ## Mixedwood : Deciduous species more than 20%
+    "Mixed"
+  } else if ((DT$leading2 %in% c("Pice_mar") &&
+              (.sumRelBs("Lari_lar", DT) >= zeroCutoff ||
+               .sumRelBs(c("Popu_tre", "Popu_bal", "Abie_bal", "Pinu_ban", "Pinu_con"), DT) < zeroCutoff))
+             ||
+             DT$leading2 == "Lari_lar")
+  { ## Wetland :Black spruce dominated and  Deciduous species more than 20%
+    "BkSpWet"
+  } else if (.sumRelBs(deciSp, DT) < 0.2) {
+    "ConMix"
+  } else if ((DT$leading2 %in% c("Popu_tre", "Popu_bal", "Betu_pap")) &&
+             .sumRelBs(c("Pinu_ban", "Lari_lar", "Pice_mar", "Pice_gla"), DT) >= zeroCutoff) { # I changed the value from 0.2 + added Pice_gla
+    "DecWet"
+  } else
+    ## just in case there are any not covered
+    NA_character_
 }
-
-
 #' internal function that sums relative biomasses for species matching a character string,
 #' but that can be appear duplicated in another species coding column.
 #' @param sppToMatch character string of species to match against for summing B.
 #' @param DT data.table with columns 'speciesCode', 'relB'.
 .sumRelBs <- function(sppToMatch, DT) {
-  browser()
-  DT[speciesCode %in% sppToMatch] %>%
-    
-    .[, .(speciesCode, relB)] %>%
-    unique(.) %>%
-    .$relB %>%
-    sum(.)
+  DT[speciesCode %in% sppToMatch, relB] %>% sum()
 }
+
+
