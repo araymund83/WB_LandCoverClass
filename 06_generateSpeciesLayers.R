095ecdb9f93cd1801740909e0a1ffdca77c74d5a
Ana Raymundo <araymund83@gmail.com>
2021-03-28 23:18:37 +0000

    feat-updated speciesLayers

--- 06_generateSpeciesLayers.R
+++ 06_generateSpeciesLayers.R
@@ -1,84 +1,80 @@
 do.call(SpaDES.core::setPaths, paths2)
+dataPrep <- list(
+  subsetDataBiomassModel = 50,
+  pixelGroupAgeClass = 20,
+  successionTimeStep = 10,
+  useCache = TRUE
+)
 
-objects2 <- list(
-  #"nonTreePixels" = simOutPreamble$nonTreePixels,
-  "omitNonVegPixels" = TRUE,
-  #"cloudFolderID" = cloudFolderID,
+dataPrepParams2001 <- list(
+  Biomass_borealDataPrep = list(
+    "biomassModel" = quote(lme4::lmer(B ~ logAge * speciesCode + cover * speciesCode +
+                                        (logAge + cover | ecoregionGroup)))
+    , "forestedLCCClasses" =  forestedLCCClasses
+    , "exportModels" = "all"
+    , "LCCClassesToReplaceNN" = LCCClassesToReplaceNN
+    , "pixelGroupAgeClass" = dataPrep$pixelGroupAgeClass
+    , "speciesUpdateFunction" = list(
+      quote(LandR::speciesTableUpdate(sim$species, sim$speciesTable,
+                                      sim$sppEquiv, P(sim)$sppEquivCol)),
+      quote(LandR::updateSpeciesTable(sim$species, sim$speciesParams))
+    )
+    , "sppEquivCol" = sppEquivCol
+    , "subsetDataBiomassModel" = dataPrep$subsetDataBiomassModel
+    , "successionTimestep" = 10
+    , "subsetDataBiomassModel" = dataPrep$subsetDataBiomassModel
+    , ".useCache" = c(".inputObjects", "init")
+   ),
+  Biomass_speciesData = list(
+    "sppEquivCol" = sppEquivCol,
+    ".studyAreaName" = paste0(studyAreaName, 2001)
+  )
+)
+
+dataPrepOutputs2001 <- data.frame(
+  objectName = c("cohortData",
+                 "pixelGroupMap",
+                 "speciesLayers",
+                 "standAgeMap",
+                 "rawBiomassMap"),
+  saveTime = 2001,
+  file = paste0 (studyAreaName, "_",
+                 c("cohortData2001_NWT.rds",
+                   "pixelGroupMap2001_NWT.rds",
+                   "speciesLayers2001_NWT.rds",
+                   "standAgeMap2001_NWT_borealDataPrep.rds",
+                   "rawBiomassMap2001_NWT_borealDataPrep.rds"))
+)
+
+dataPrepObjects <- list(
   "rasterToMatch" = rasterToMatch,
   "rasterToMatchLarge" = rasterToMatchLarge,
   "sppColorVect" = sppColorVect,
   "sppEquiv" = sppEquiv,
- # "speciesLayers" = speciesLayers2001,
   "studyArea" = studyArea,
-  "studyAreaLarge" = studyAreaLarge,
-  #"studyAreaReporting" = simOutPreamble$studyAreaReporting,
-  "rstLCC " = rstLCC,
-  "vegMap" = vegMap,
-  "firePoints" = firePoints,
-  "standAgeMap" = standAgeMap2011,
-  "rawBiomassMap" = rawbiomassMap2001,
-  "flammableMap" = flammableMap,
-  "ecoregions" = ecoregionsMap
+  "studyAreaLarge" = studyAreaLarge
+#  "rstLCC " = rstLCC,
+ # "vegMap" = vegMap,
+  #"firePoints" = firePointsNWT,
+  #"standAgeMap" = standAgeMap2011,
+  #"rawBiomassMap" = rawbiomassMap2001,
+  #"flammableMap" = flammableMap,
+  #"ecoregions" = ecoregionsMap
 )
 
-parameters2 <- list(
- Biomass_speciesData = list(
-      "sppEquivCol" = sppEquivCol,
-      "types" = "KNN"
-  ),
- Biomass_borealDataPrep = list(
-    "sppEquivCol" = sppEquivCol,
-    "speciesUpdateFunction" = list(
-      quote(LandR::speciesTableUpdate(sim$species, sim$speciesTable,
-                                       sim$sppEquiv, P(sim)$sppEquivCol)),
-      quote(LandR::updateSpeciesTable(sim$species, sim$speciesParams))
-     ) ## ### MAKE SURE of closing the list in the right plaCE!!!!!!!
-     , "biomassModel" = quote(lme4::lmer(B ~ logAge * speciesCode + cover * speciesCode +
-                                           (logAge + cover | ecoregionGroup)))
-     , "forestedLCCClasses" =  forestedLCCClasses
-     , "LCCClassesToReplaceNN" = LCCClassesToReplaceNN
-     , "pixelGroupAgeClass" = dataPrep$pixelGroupAgeClass
-     #, ".useCache" = c(".inputObjects", "init")
-#     #, "runName" = runName
-     ,"standAgeMap" = standAgeMap2011
-     , "rstLCC" = rstLCC
-     , "rawBiomassMap" = rawbiomassMap2001
-#     , "speciesLayers" = speciesLayers2001
-     , "successionTimestep" = 10
-     , "subsetDataBiomassModel" = 10
-     #, "useCloudCacheForStats" = TRUE
-     , ".useCache" = c(".inputObjects", "init")
-     , "exportModels" = "all"
-     , "pixelGroupAgeClass" = 10
-   ))
+sppLayersFile <- file.path(Paths$outputPath, paste0("biomassMaps2001_",
+                                                    studyAreaName, ".qs"))
 
-dataPrepOutputs2001 <- data.frame(objectName = c("cohortData",
-                                                 "pixelGroupMap",
-                                                 "speciesLayers",
-                                                 "standAgeMap",
-                                                 "rawBiomassMap"),
-                                  saveTime = 2001,
-                                  file = c("cohortData2001_ABBC.rds",
-                                           "pixelGroupMap2001_ABBC.rds",
-                                           "speciesLayers2001_ABBC.rds",
-                                           "standAgeMap2001_borealDataPrep.rds",
-                                           "rawBiomassMap2001_borealDataPrep.rds"))
-
-sppLayersFile <- file.path(Paths$outputPath, paste0("simOutSpeciesLayers_",
-                                                   studyarea, ".qs"))
-
-speciesModules <- c("Biomass_speciesData", "Biomass_borealDataPrep")
-
-simOutSppLayers <- Cache(simInitAndSpades,
-                         times = list(start = simTimes$start, end = simTimes$start + 1)
-                         , params = parameters2
-                         , modules = speciesModules
-                         , objects = objects2
+biomassMaps2001 <- reproducible::Cache(simInitAndSpades,
+                         times = list(start = 2001, end = 2001)
+                         , params = dataPrepParams2001
+                         , modules = list( "Biomass_speciesData", "Biomass_borealDataPrep")
+                         , objects = dataPrepObjects
                          , outputs = dataPrepOutputs2001
                          , paths = getPaths()
+                         , loadOrder = c("Biomass_speciesData", "Biomass_borealDataPrep")
                          , debug = TRUE
-                         , .plotInitialTime = NA
-                         , loadOrder = unlist(speciesModules)
+                         , .plots = NA
+                         , userTags = c("dataPrep2001", "studyAreaName")
 )
-saveSimList(simOutSppLayers, sppLayersFile)
-outSimSppLayers <- qs::qread((file.path(Paths$inputPath, "simOutSpeciesLayers_ABBC.qs")))
+saveSimList(sim = biomassMaps2001, filename= sppLayersFile)
