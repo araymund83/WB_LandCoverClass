92ad70355fe573f0e6378c34159a5e330f2b979b
araymund83 <araymund83@gmail.com>
2021-04-09 19:35:58 +0000

    feat-07_ vegReclass added

--- 00-global.R
+++ 00-global.R
@@ -6,7 +6,7 @@ if (!require("Require")) {
 Require("PredictiveEcology/SpaDES.install")
 #installSpaDES() ## TODO: fix -- see SpaDES.install#7 and SpaDES.install#8
 
-studyarea <- "bcr6NWT"
+studyarea <- "bcr6AB"
 studyAreaName <- studyarea
 
 source("01_init.R")
@@ -15,5 +15,6 @@ source("03_packages.R")
 source("04_options.R")
 source("05_preamble.R")
 source("06_generateSpeciesLayers.R")
+source("07_vegReclass.R")
 source("07_borealDataPrep.R")
 #source("08_mainSim.R")
