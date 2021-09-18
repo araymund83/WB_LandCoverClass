382dee91256bee635960e82aefdc217180fd1e30
araymund83 <araymund83@gmail.com>
2021-09-17 22:46:19 -0700

    feat-extract cohortData for specific bcr

--- extractPixelID.R
+++ extractPixelID.R
@@ -7,20 +7,34 @@ biomassMaps2011MB<- qs::qread(file.path(getwd(),"inputs", "biomassMaps2011_MB.qs
 bcrMBpixels<- raster::extract(biomassMaps2011MB$pixelGroupMap,
                               simOutPreamble$studyAreaReporting,
                               cellnumbers = TRUE, df = TRUE)
-unique(bcrMBpixels$polyID)
-
 
 ##assign the BCR number to the ID
 bcrMBpixels$ID <- as.factor(bcrMBpixels$ID)
 simOutPreamble$studyAreaReporting$BCR <- as.factor(simOutPreamble$studyAreaReporting$BCR)
 levels(bcrMBpixels$ID) <- levels(simOutPreamble$studyAreaReporting$BCR)
 
-names(bcrMBpixels) <- c ("polyID", "pixelID", "pixelGroupMap")
+## rename columns for easy reference
+names(bcrMBpixels) <- c ("bcrID", "pixelID", "pixelGroup")
+
+##merge both DT to have the BCR id in the cohortData
+bcrCohortData<- merge(pixelCohortData2, bcrMBpixels, by = "pixelGroup")
+
+
+pixelCohortData <- LandR::addPixels2CohortData(cohortData,
+                                               biomassMaps2011MB$pixelGroupMap,
+                                               doAssertion = getOption("LandR.assertions", TRUE))
+pixelCohortData2 <- LandR::addNoPixel2CohortData(cohortData,
+                                               biomassMaps2011MB$pixelGroupMap,
+                                               doAssertion = getOption("LandR.assertions", TRUE))
+
+
+bcrSelect <- 6
+subsetPixels<- bcrMBpixels[bcrMBpixels$bcrID %in% bcrSelect, ]
 
 plot(biomassMaps2011MB$pixelGroupMap)
 #for some reason cohortData loads as a list, so it needs to be reconverted
 cohortData <- as.data.table(biomassMaps2011MB$cohortData)
-bcrSApixels <- bcrMBpixels[bcrMBpixels$polyID == unique(bcrMBpixels$polyID), ]
+bcrSApixels <- bcrMBpixels[bcrMBpixels$bcrID == unique(bcrMBpixels$bcrID), ]
 
 
 # tempRas <- copy(biomassMaps2011MB$pixelGroupMap)
@@ -29,17 +43,12 @@ bcrSApixels <- bcrMBpixels[bcrMBpixels$polyID == unique(bcrMBpixels$polyID), ]
 cohorDataShort <- lapply(cohortData, unique)
 
 
-cohortDataShort <- cohortData[cohortData$pixelGroup %in% bcrSApixels$pixelGroupMap]
+cohortDataShort <- pixelCohortData[pixelCohortData$pixelGroup %in% bcrSApixels$pixelGroupMap]
 all(cohortDataShort$pixelGroup %in% cohortData$pixelGroup)
 all(cohortDataShort$pixelGroup %in% pixelCohortData$pixelGroup)
+all(bcrMBpixels$pixelID %in% pixelCohortData$pixelIndex)
+all(pixelCohortData$pixelGroup %in% bcrMBpixels$pixelGroup)
 
-pixelCohortData <- LandR::addPixels2CohortData(cohortDataShort,
-                                               biomassMaps2011MB$pixelGroupMap,
-                                               doAssertion = getOption("LandR.assertions", TRUE))
-
-
-
-cohortDataSA <-
 
 
 
